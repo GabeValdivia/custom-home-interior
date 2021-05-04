0f5dd6bf2eaf9b3751de5962f8ebc2e84f3e74fe
Gabe Valdivia <gabe@gabeswebcoding.com>
2021-05-04 01:43:14 -0500

    Add missing image on homeblock 4 via get_template_directory_uri() php

--- themes/home-interior/index.php
+++ themes/home-interior/index.php
@@ -183,7 +183,7 @@
     <div class="container py-lg-5 py-md-4">
         <div class="row">
             <div class="col-lg-6 ">
-                <img src="assets/images/s2.jpg" class="img-fluid radius-image" alt="">
+                <img src="<?php echo get_template_directory_uri().'/assets/images/s2.jpg'; ?>" class="img-fluid radius-image" alt="">
             </div>
             <div class="col-lg-6 align-self mt-lg-0 mt-4">
                 <div class="row mb-sm-4 mb-2 pb-3">
